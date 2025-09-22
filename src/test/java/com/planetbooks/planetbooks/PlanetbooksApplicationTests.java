1beddc2a726bf31e99ae0eb6ae26a2b3cc984685
Daniela Ojeda <165310716+DanielaFernandaOjedaArrelucea@users.noreply.github.com>
2025-09-22 18:05:52 -0500

    inicializar proyecto en springboot

---
+++ src/test/java/com/planetbooks/planetbooks/PlanetbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planetbooks.planetbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
